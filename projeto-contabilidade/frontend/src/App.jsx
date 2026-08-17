b07b1294c5e979dd9e57332ce72aa09a178ecfd6
Thomas Freire <thomasfreirecontato@gmail.com>
2026-08-17 19:07:22 -0300

    Update App.jsx

--- projeto-contabilidade/frontend/src/App.jsx
+++ projeto-contabilidade/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './index.css';
+import './App.css';
 
 export default function App() {
   const [formData, setFormData] = useState({ nome: '', email: '', telefone: '', mensagem: '' });
